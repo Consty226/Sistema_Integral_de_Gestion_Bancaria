04a33e30708b436dc7840b91106f9488440b4eb5
Constancio Otaola Macagno <ConstancioOtaola@gmail.com>
2025-05-26 19:00:29 -0300

    Implementacion correcta de abstraccion en Cliente y sus subclases

--- src/Clientes/Empresa.java
+++ src/Clientes/Empresa.java
@@ -1,14 +1,23 @@
 package Clientes;
 
+import CajaDeSeguridad.CajaDeSeguridad;
 import java.util.List;
 
 public class Empresa extends Cliente {
     private String cuit;
 
-
     public Empresa(String nombre, String direccion, List<String> telefonos, String cuit, String claveCaja) {
-        super(nombre, direccion, telefonos, claveCaja);
+        setNombre(nombre);
+        setDireccion(direccion);
+
+        for (String telefono : telefonos) {
+            agregarTelefono(telefono);
+        }
+
         this.cuit = cuit;
+
+        CajaDeSeguridad caja = new CajaDeSeguridad(this, claveCaja);
+        setCajasDeSeguridad(caja);
     }
 
     public String getCuit() {
@@ -18,5 +27,9 @@ public class Empresa extends Cliente {
     public void setCuit(String cuit) {
         this.cuit = cuit;
     }
-}
 
+    @Override
+    public String tipoCliente() {
+        return "Empresa";
+    }
+}
