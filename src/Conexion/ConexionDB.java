bd179a970b3e1b285794a90aaeb251137729dd92
Constancio Otaola Macagno <ConstancioOtaola@gmail.com>
2025-05-30 12:18:32 -0300

    Primer commit de la clase ConexionDB y el archivo para crear la base de datos

---
+++ src/Conexion/ConexionDB.java
@@ -0,0 +1,20 @@
+package Conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionDB {
+    private static final String URL = "jdbc:mysql://localhost:3306/banco_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection conectar() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Error de conexión: " + e.getMessage());
+            return null;
+        }
+    }
+}
